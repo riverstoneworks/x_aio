e361d14a58efecb412df142a2e2bb8b41321a5ef
sxpc722 <sxpc722@aliyun.com>
2017-10-25 22:12:38 +0800

    add extern before aio_service_start

--- src/include/aio_service.h
+++ src/include/aio_service.h
@@ -21,7 +21,7 @@ typedef struct AIO_PARAM{
 	void *(*on_io_complete) (struct io_event *,int n); //how to deal the eventsQueue exported by io_getevents
 }Aio_param;
 
-const char* aio_service_start(const Aio_param * const ap,cpu_set_t* cpuset) __THROWNL __nonnull ((1));;
+extern const char* aio_service_start(const Aio_param * const ap,cpu_set_t* cpuset) __THROWNL __nonnull ((1));;
 extern int submit_io_task(char* aiop,int fd, unsigned io_cmd, void* data, void(*callback)(void*),__u64 buf,__s64 offset) __THROWNL __nonnull ((1,4,5));
 extern int aio_service_stop(const char* const aiop) __THROWNL __nonnull ((1));;
 
